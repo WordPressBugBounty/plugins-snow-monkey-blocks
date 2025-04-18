4acfc492fd8ab60c6683d94085d0b8b2ba9c6d9f
Sean Murphy <sean@iamseanmurphy.com>
2025-04-18 15:17:48 +0000

    24.0.1

--- snow-monkey-blocks/dist/blocks/list/index.asset.php
+++ snow-monkey-blocks/dist/blocks/list/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'ab23fa58a77b5a0d5c81');
+<?php return array('dependencies' => array('lodash', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-hooks', 'wp-i18n'), 'version' => '01bbc8717f139423e36c');
